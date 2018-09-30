070759b62505f9a50307180fc99be508b02bfdbc
Ana María Espinosa <am.espinosa11@uniandes.edu.co>
2018-09-30 16:17:37 -0500

    assign user to workflow v1

--- reds/urls.py
+++ reds/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from resources import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name="index"),
+    path('workflow/users', views.workflow_users, name="workflow_users" )
 ]
