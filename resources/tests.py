bee8dbb4cf1c61ef52da4b0f8fe7392862836700
Juan José Villegas <jj.villegas47@gmail.com>
2018-11-21 21:10:00 -0500

    Added resource test and removed all others

--- resources/tests.py
+++ resources/tests.py
@@ -7,6 +7,9 @@ import platform
 from resources.models import *
 from users.models import User
 from selenium.webdriver.support.wait import WebDriverWait
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.urls import reverse
 
 
 class ResourceTestCase(LiveServerTestCase):
@@ -150,3 +153,28 @@ class ArtifactTestCase(LiveServerTestCase):
         name_label = self.selenium.find_element_by_xpath('/html/body/div/div/div[1]/div[1]/span/strong')
 
         self.assertIn('Artefactos del recurso', name_label.text)
+
+
+class ResourceTests(APITestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        Project.objects.create(name='Projecto de prueba', description='Lo que sea')
+
+    def test_create_resource(self):
+        url = 'https://reds-miso.herokuapp.com/api/resources/'
+        data = {
+            "current_phase": 1,
+            "description": "aldjf aslfj lasdf as",
+            "estimated_duration": 23,
+            "name": "Segundo recurso",
+            "priority": 2,
+            "project_id": Project.objects.first().pk,
+            "type": "Web page",
+            "tags": ["algo", "segundo", "tercero"]
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Resource.objects.count(), 1)
+        self.assertEqual(Resource.objects.get().name, 'Segundo recurso')
