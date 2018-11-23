93d1cdafce726810f560931d304efdc80506f1bf
Sergio Velasquez <sy.velasquez10@uniandes.edu.co>
2018-11-22 21:51:35 -0500

    Busquedas por cada columna

--- frontend/src/app/components/find-artifacts/pipe.ts
+++ frontend/src/app/components/find-artifacts/pipe.ts
@@ -1,17 +1,31 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-    name: 'artifactFilter',
-    pure: false
-  })
-  export class MyFilterPipe implements PipeTransform {
-    constructor() { }
-    transform(value: any, query: string): any {
-        return query ? value.reduce((prev, next) => {
-          if (next['name'].includes(query) || next['description'].includes(query) || next['tags'] && next['tags'].includes(query)) {
-            prev.push(next);
-          }
-          return prev;
-        }, []) : value;
+  name: 'artifactFilter',
+  pure: false
+})
+export class MyFilterPipe implements PipeTransform {
+  constructor() { }
+  transform(value: any, query: string, type: string): any {
+    return query ? value.reduce((prev, next) => {
+      if (type === 'all') {
+        if (next['name'].includes(query) || next['description'].includes(query) || next['tags'] && next['tags'].includes(query)) {
+          prev.push(next);
+        }
+      } else if (type === 'name') {
+        if (next['name'].includes(query)) {
+          prev.push(next);
+        }
+      } else if (type === 'description') {
+        if (next['description'].includes(query)) {
+          prev.push(next);
+        }
+      } else if (type === 'tags') {
+        if (next['tags'] && next['tags'].includes(query)) {
+          prev.push(next);
+        }
       }
+      return prev;
+    }, []) : value;
   }
+}
